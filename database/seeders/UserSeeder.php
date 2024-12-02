1e8fc2e7935b5f67c0159ae1050393928f773c9a
CDevFlttr <marraffacinzia5@gmail.com>
2024-12-02 11:16:26 +0100

    update

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        // create demo users
+      \App\Models\User::factory()->create([
+        'name' => 'Admin',
+        'email' => 'studiogaglio@gmail.com',
+        'password' => 'password',
+    ]);
+
+
+    }
+}
